0ce75707b8da60bcdc8cd6c27ee43ad20697e6f0
Nicolas Brousse <nicolas@pantographe.studio>
2017-05-26 21:32:23 +0200

    Remove DSL::MethodHelpers and move onto DSL

--- lib/capistrano/procfile/dsl.rb
+++ lib/capistrano/procfile/dsl.rb
@@ -4,14 +4,41 @@ require "capistrano/procfile/options"
 require "capistrano/procfile/utils"
 require "capistrano/procfile/dsl/apply_helpers"
 require "capistrano/procfile/dsl/management_helpers"
-require "capistrano/procfile/dsl/method_helpers"
 
 module Capistrano
   module Procfile
     module DSL
       include ApplyHelpers
       include ManagementHelpers
-      include MethodHelpers
+
+      def procfile
+        procfile = fetch(:procfile, nil)
+      end
+
+      def procfile_service_name
+        Utils.parameterize(fetch(:procfile_service_name))
+      end
+
+      def wait_until(timeout, msg)
+        return unless timeout > 0
+
+        puts msg % timeout
+        sleep timeout
+      end
+
+      def rounded_on(servers, force: false, &block)
+        # @todo add an option to enable this or not
+        #       because in most of the case we could configure our capistrano
+        #       to send correct signals.
+        #       But it could be nice to have the avaibility of using this? no?
+        opts = Hash.new
+
+        if force || fetch(:my_option, false)
+          opts = { in: :groups, limit: (servers.length / 3).round }
+        end
+
+        on servers, opts, &block
+      end
 
     private
 
